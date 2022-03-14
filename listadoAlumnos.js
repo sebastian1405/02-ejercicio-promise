733d2e1a4c895a5563f7b55436ec4076c30ca2da
Sebastian Guevara <sebastianguevara32569830@gmail.com>
2022-03-14 19:14:31 -0300

    con-promise

--- listadoAlumnos.js
+++ listadoAlumnos.js
@@ -1,20 +1,40 @@
 const fs = require("fs");
-
 const obj = {};
 obj.students = [];
-for (let i = 1; i <= 5; i++) {
-  fs.readFile(`./files/alumnos${i}.json`, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      a = JSON.parse(data);
-      obj.students.push(a);
-      json = JSON.stringify(obj, null, 2);
-      fs.writeFile("./files/listadoAlumnos.json", json, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-  });
-}
+const alumnos = new Promise((resolve, reject) => {
+  for (let i = 1; i <= 5; i++) {
+    fs.readFile(`./files/alumnos${i}.json`, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        a = resolve(JSON.parse(data));
+        obj.students.push(a);
+        json = JSON.stringify(obj, null, 2);
+        fs.writeFile("./files/listadoAlumnos.json", json, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+  }
+});
+
+// const obj = {};
+// obj.students = [];
+// {
+//   fs.readFile(`./files/alumnos${i}.json`, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       a = JSON.parse(data);
+//       obj.students.push(a);
+//       json = JSON.stringify(obj, null, 2);
+//       fs.writeFile("./files/listadoAlumnos.json", json, (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     }
+//   });
+// }
